010426b2adb35b3966a507a0c5c591173376a431
VinaySingla1235 <102358007+VinaySingla1235@users.noreply.github.com>
2022-05-09 21:52:09 +0530

    Imported props and state from react class based components using bootstrap cards to display news snippets and made basic structure of news snippets

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,13 +1,27 @@
-import React, { Component } from 'react'
+//import { getByTitle } from "@testing-library/react";
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
+    let {title,description,imageUrl,newsUrl} = this.props;
     return (
+      
       <div>
-        This is a news item component
+        <div className="card" style={{width: "18rem"}}>
+          <img src={imageUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">
+              {description}
+            </p>
+            <a href="/about" className="btn-sm btn-primary">
+              Read More
+            </a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
