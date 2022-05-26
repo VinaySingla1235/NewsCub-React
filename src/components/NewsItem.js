46f937bebfac9286dedf593941286a503168ea38
VinaySingla1235 <102358007+VinaySingla1235@users.noreply.github.com>
2022-05-26 21:44:45 +0530

    Added previous and next button

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,13 +8,13 @@ export class NewsItem extends Component {
       
       <div>
         <div className="card" style={{width: "18rem" ,height:"25rem"}}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img src={imageUrl} className="card-img-top" style={{maxWidth:"18rem",maxHeight:"10rem"}} alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">
               {description}
             </p>
-            <a href={newsUrl} target="_blank" className="btn-sm btn-primary">
+            <a href={newsUrl} target="_blank" className="btn-sm btn-primary" style={{textDecoration:"none"}}>
               Read More
             </a>
           </div>
