1ac8e197ad236a16afdeb2f8725d8a9710ae778f
VinaySingla1235 <102358007+VinaySingla1235@users.noreply.github.com>
2022-05-29 18:04:31 +0530

    Added fully working categories option without react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,46 +3,29 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  
-} from "react-router-dom";
+
 export default class App extends Component {
   constructor(){
     super();
     this.state={
-      category:'general'
+      category:'general',
+      key:'general',
     }
     
   }
-  // changeCategory=(newCategory)=>{
-  //   this.setState({
-  //     category:newCategory
-  //   })
-  //   console.log(`Category changed to ${this.state.category}`);
-  // }
+  changeCategory=(newCategory)=>{
+    this.setState({
+      category:newCategory,
+      key:newCategory
+    })
+    console.log(`Category changed to ${this.state.category}`);
+  }
   render() {
     return (
       <div>
-       <Router>
-       <Navbar category={this.state.category}/>
-       <Routes>
-          <Route exact path="/" element={<News key="general" category="general"  country="in"/>}/>
-          <Route exact path="/business" element={<News key="business" category="business"   country="in"/>}/>
-          <Route exact path="/entertainment" element={<News key="entertainment" category="entertainment"  country="in"/>}/>
-          <Route exact path="/health" element={<News key="health" category="health"  country="in"/>}/>
-          
-          <Route exact path="/science" element={<News key="science" category="science"   country="in"/>}/>
-          
-          <Route exact path="/sports" element={<News key="sports" category="sports"  country="in"/>}/>
-          
-        
-          <Route exact path="/technology" element={<News key="technology" category="technology"  country="in"/>}/>
-          
-       </Routes>
-       </Router>
+       
+       <Navbar category={this.state.category} changeCategory={this.changeCategory}/>
+       <News key={this.state.key} category={this.state.category}  country="in"/>
       </div>
     )
   }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react'
 //import PropTypes from 'prop-types'
-import {
-  Link,
-  //useLocation
-} from "react-router-dom";
 export class Navbar extends Component {
   
   render() {
@@ -11,26 +7,26 @@ export class Navbar extends Component {
     return (
         <nav className="navbar navbar-expand-lg bg-light navbar-light">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">NewsMonkey</Link>
+          <a className="navbar-brand" href="/">NewsMonkey</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link" aria-current="page" to="/">Home</Link>
-              </li>
+            <li className="nav-item">
+          <a className="nav-link" aria-current="page" href="/">Home</a>
+        </li>
               <li className="nav-item dropdown">
-          <Link className="nav-link dropdown-toggle" to="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Categories
-          </Link>
+          </a>
           <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><Link className="dropdown-item" to="business"> Business</Link></li>
-            <li><Link className="dropdown-item" to="entertainment"> Entertainment</Link></li>
-            <li><Link className="dropdown-item" to="health"> Health</Link></li>
-            <li><Link className="dropdown-item" to="science"> Science</Link></li>
-            <li><Link className="dropdown-item" to="sports"> Sports</Link></li>
-            <li><Link className="dropdown-item" to="technology"> Technology</Link></li>
+            <li className="dropdown-item" onClick={()=>{this.props.changeCategory('business')}} >Business</li>
+            <li className="dropdown-item" onClick={()=>{this.props.changeCategory('entertainment')}} >Entertainment</li>
+            <li className="dropdown-item" onClick={()=>{this.props.changeCategory('health')}} >Health</li>
+            <li className="dropdown-item" onClick={()=>{this.props.changeCategory('science')}} >Science</li>
+            <li className="dropdown-item" onClick={()=>{this.props.changeCategory('sports')}} >Sports</li>
+            <li className="dropdown-item" onClick={()=>{this.props.changeCategory('technology')}} >Technology</li>
           </ul>
         </li>
             </ul>
