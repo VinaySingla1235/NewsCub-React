225eb2705d63ef677059cc1c730a535e45bf6a5e
VinaySingla1235 <102358007+VinaySingla1235@users.noreply.github.com>
2022-05-31 11:26:32 +0530

    Saved user choosen categories and countries in local storage

--- src/App.js
+++ src/App.js
@@ -5,29 +5,45 @@ import Navbar from './components/Navbar';
 import News from './components/News';
 
 export default class App extends Component {
+  fetchFromStorage=(parameter,defaultpara)=>{
+  if(localStorage.getItem(parameter)==null){
+    return defaultpara;
+  }
+  else{
+    return localStorage.getItem(parameter);
+  }
+
+  }
   constructor(){
     super();
     this.state={
-      category:'general',
-      key:'India-general',
-      country:'in',
-      countryName:'India'
+      category:this.fetchFromStorage('newsAppCategory','general'),
+      key:this.fetchFromStorage('newsAppKey','India-general'),
+      country:this.fetchFromStorage('newsAppCountry','in'),
+      countryName:this.fetchFromStorage('newsAppCountryName','India')
     }
     
   }
   changeCategory=(newCategory)=>{
+    let newKey=`${this.state.countryName}-${newCategory}`;
     this.setState({
       category:newCategory,
       key:`${this.state.countryName}-${newCategory}`
     })
+    localStorage.setItem('newsAppCategory',newCategory);
+    localStorage.setItem('newsAppKey',newKey);
     console.log(`Category changed to ${this.state.category}`);
   }
   changeCountry=(countryCode,countryNameparam)=>{
+    let newKey=`${countryNameparam}-${this.state.category}`;
     this.setState({
       country:countryCode,
       countryName:countryNameparam,
       key:`${countryNameparam}-${this.state.category}`
     })
+    localStorage.setItem('newsAppCountry',countryCode);
+    localStorage.setItem('newsAppCountryName',countryNameparam);
+    localStorage.setItem('newsAppKey',newKey);
   }
   render() {
     return (
